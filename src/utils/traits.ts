c5531dc3c4a1ff76d4d7997285cc3ab966c6f9f3
Chris Miller <devchrismiller@gmail.com>
2025-02-04 21:44:48 -0500

    Implemented trait data with breakpoints

---
+++ src/utils/traits.ts
@@ -0,0 +1,455 @@
+// TODO: Move to Database along with units.ts
+// 1 = Bronze, 2 = Silver, 3 = Gold, 4 = Platinum (Can't create in team builder), 5 = Unique
+
+export const TraitsArray = [
+  {
+    id: 1,
+    name: "academy",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 5,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "ambusher",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 3,
+        level: 2,
+      },
+      {
+        numUnits: 5,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "artillerist",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "automata",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "banished mage",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 5,
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "black rose",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 5,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "blood hunter",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 5,
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "bruiser",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "chem-baron",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 5,
+        level: 3,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "conqueror",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "dominator",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "emmissary",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: "enforcer",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: "experiment",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 1,
+      },
+      {
+        numUnits: 5,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: "family",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: "firelight",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 3,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: "form swapper",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 18,
+    name: "high roller",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 5,
+      },
+    ],
+  },
+  {
+    id: 19,
+    name: "junker king",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 5,
+      },
+    ],
+  },
+  {
+    id: 20,
+    name: "machine herald",
+    breakpoints: [
+      {
+        numUnits: 1,
+        level: 5,
+      },
+    ],
+  },
+  {
+    id: 21,
+    name: "pit fighter",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 22,
+    name: "quickstriker",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 3,
+        level: 2,
+      },
+      {
+        numUnits: 4,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 23,
+    name: "rebel",
+    breakpoints: [
+      {
+        numUnits: 3,
+        level: 1,
+      },
+      {
+        numUnits: 5,
+        level: 2,
+      },
+      {
+        numUnits: 7,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 24,
+    name: "scrap",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 25,
+    name: "sentinel",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 26,
+    name: "sniper",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 27,
+    name: "sorcerer",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 28,
+    name: "visionary",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+  {
+    id: 29,
+    name: "watcher",
+    breakpoints: [
+      {
+        numUnits: 2,
+        level: 1,
+      },
+      {
+        numUnits: 4,
+        level: 2,
+      },
+      {
+        numUnits: 6,
+        level: 3,
+      },
+    ],
+  },
+];
